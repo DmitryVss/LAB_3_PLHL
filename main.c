ed4a0c5de3dddbf68d64cfc65f0a076071f06dd0
Dmitriy Volkov <dmitrij_volkov_2010@mail.ru>
2019-10-31 17:58:15 +0300

    2.0 the second version of program. '\t' must be in the beginning of line

--- main.c
+++ main.c
@@ -1,45 +1,71 @@
 /******************************************************************
-*             ИНФОРМАТИКА И КОМПЬЮТЕРНЫЕ ТЕХНОЛОГИИ               *
+*               Informatics & Computer Technologies               *
 *******************************************************************
-*                    Лабораторная работа # 3                      *
+*                           LAB # 3                               *
 *******************************************************************
 * Project Type :win 32 Console Application                        *
 * Project Name :LAB_3_PLHL                                        *
 * File Name    :main.c                                            *
 * Language     :C MVS 2017                                        *
-* Programmers  :Дмитрий Волков, Алексей Толочный                  *
+* Programmers  :Dmitry Volkov, Alexey Tolochny                    *
 * Group        :М3О-208Б-18                                       *
-* Modified by  :Дмитрий Волков                                    *
+* Modified by  :Dmitrij Volkov                                    *
 * Created      :18.10.19                                          *
-* Last Revision:19.10.19                                          *
-* Comment      : программа выравнивает текст по центру до длины   *
-* (N + delta), где N - длина максимальной строки исходного текста *
-*                  delta - задается пользователем (>0);           *
-* '\t' заменяется на пять пробелов                                *
+* Last Revision:31.10.19                                          *
+* Comment      : the program centers text, (N + delta) - size of modified string  *
+*                  N - length of max string in sourse txt         *
+*                  delta - setting by user (>0);                  *
+*                  '\t' replased by 5 spaces                      *
 *******************************************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
-int i = 0;				//счетчик
-const int NMAX = 100;	//максимальная длина буфера обмена
-/******************************************************************/
-int dmxline(FILE*);			//определение длины максимальной строки
-void cler_str(char*,		//очистка строки
-	int,				//индекс, с которого идет очистка
-	int);				//индекс, до которого идет очистка
-/******************************************************************/
+short i = 0;	//counter
+/*************************** prototypes ***************************/
+//determine of max line
+int dmxline(FILE* in)
+{
+	short dN = 0;		//right border
+	short dtemp = 0;	//var for compare
+	char dc = 0;		//var for reading from the file
+
+	while (dc != EOF)
+	{
+		dtemp = 0;
+		dc = 0;	
+		while ((dc != '\n') && (dc != EOF))
+		{
+			dc = fgetc(in);
+			if (dc == '\t')	dtemp += 5;
+			else dtemp++;
+			if ((dc == '\n') || (dc == EOF)) dtemp++;
+		}
+		if (dtemp > dN)	dN = dtemp;
+	}
+	return dN;
+}
+//cleaning of char*
+void cler_line(char* cArr, int cN)
+{
+	for (i = 0; i < cN; i++) cArr[i] = '\0';
+}
+//put spaces into char*
+void put_sp(char* pArr, int pN)
+{
+	for (i = 0; i < pN; i++) pArr[i] = ' ';
+}
+/**************************** main() ******************************/
 int main()
 {
 	FILE *input = fopen("C:\\Users\\dmitr\\Desktop\\ПЯВУ\\3 СЕМ\\ЛР_3 ПЯВУ\\LAB_3_PLHL\\LAB_3_PLHL\\data.txt", "rt");
-	FILE *output = fopen("C:\\Users\\dmitr\\Desktop\\ПЯВУ\\3 СЕМ\\ЛР_3 ПЯВУ\\LAB_3_PLHL\\LAB_3_PLHL\\res.txt", "wt");
-	int N = -1;		//правая граница
-	int el = -1;	//индекс конца строки
-	int delta = -1;	//переменная задается пользоваетелем (delta > 0)
-	char* clp;		//основной буфер обмена
-	char* temp;		//временный буфер для выравнивания строки
+
+	char* clp;		//clipboard
+	char* spaces;	//
+	short N;		//size of clp 
+	short delta;	//
+	short numsp;	//number of spaces
 
 	if (input == NULL)
 	{
@@ -54,115 +80,48 @@ int main()
 		system("pause");
 		return 2;
 	}
+	else rewind(input);
+
+	printf("Add the delta\n");
+	scanf("%hu", delta);
+	if (delta < 0)
+	{
+		printf("ERROR_3: delta must be > 0");
+		system("pause");
+		return 3;
+	}
 
 	N = dmxline(input);
 	N = N + delta;
 
-	clp = malloc(NMAX);
-	temp = malloc(NMAX);
+	clp = malloc(N);
+	spaces = malloc(N);
 
-	do 
+	do
 	{
+		cler_line(clp, N);
+		cler_line(spaces, N);
+
 		fgets(clp, N, input);
-		clp = strcat(clp, temp);
-		cler_str(temp, 0, strlen(temp));
-
-		//строка по размеру
-		if (strlen(clp) == N)
+		if (clp[0] == '\t')
 		{
-			fputs(clp, output);
+			clp[0] = ' ';
+			clp = strcat("    ", clp);
 		}
-		//строка меньше
-		else if (strlen(clp) < N)
-		{
-			if (feof(input) == 0)//если еще не конец файла
-				strcat(clp, " ");
+		numsp = N - strlen(clp);
+		numsp = numsp / 2;
+		put_sp(spaces, numsp);
 
-			strcat(temp, clp);
+		clp = strcat(clp, spaces);
+		clp = strcat(spaces, clp);
+		if (strlen(clp) != N) clp = strcat(clp, " ");
 
-			if (feof(input) != 0)//если конец файла
-			{
-				fputs(temp, output);
-				cler_str(temp, 0, strlen(temp));
-			}
-		}
-		//строка больше
-		else
-		{
-			el = strrchr(clp, ' ');
-			//el = N;
-			//while ((clp[el] != ' ')&&(el != 0))
-			//	el--;
-			if (el == NULL)
-			{
-				
-				//clp.erase(el, 1);
-
-				//tmpl = clp.substr(el, clp.size());
-				if (feof(input) == 0)
-					strcat(temp, " ");
-				//clp.erase(el, clp.size());
-
-				el = size_str(clp);
-				while (el != 0)
-					el--;
-			}
-			else
-			{
-				el = 0;
-				while ((clp[el] != ' ') && (el != size_str(clp)))
-					el++;
-
-			}
-		}
-		cler_str(temp, 0, strlen(temp));
-
+		printf("%s", clp);
 	} while (clp != EOF);
 
 	fclose(input);
-	fclose(output);
 
 	system("pause");
 	return 0;
 }
-
-int dmxline(FILE* in)
-{
-	int dN = 0;		//правая граница
-	int dtemp = 0;	//переменная для сравнения
-	char dc = 0;		//переменная для считывания из файла
-
-	while (dc != EOF)
-	{
-		dtemp = 0;
-		dc = 0;
-		while ((dc != '\n')&&(dc != EOF))
-		{
-			dc = fgetc(in);
-			dtemp++;
-			if ((dc == '\n')||(dc == EOF))
-				dtemp++;
-		}
-		if(dtemp > dN)
-			dN = dtemp;
-	}
-	return dN;
-}
-
-//void cler_str(char *cArr, int cB, int cE)
-//{
-//	if (cE != cB)
-//	{
-//		for (i = cB; i < cE; i++)
-//		{
-//			cArr[i] = '\0';									
-//		}
-//	}
-//	else
-//	{
-//		for (i = cB; i < strlen(cArr); i++)
-//		{
-//			cArr[i] = cArr[i++];
-//		}
-//	}
-//}
+/*************************** end of main() ************************/
