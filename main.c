074200b602691b14c9bd21eb1fb1804f950dbd5d
Dmitriy Volkov <dmitrij_volkov_2010@mail.ru>
2019-11-01 00:44:34 +0300

    2.3

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 * Language     :C MVS 2017                                        *
 * Programmers  :Dmitry Volkov, Alexey Tolochny                    *
 * Group        :Ì3Î-208Á-18                                       *
-* Modified by  :Dmitrij Volkov                                    *
+* Modified by  :Dmitry Volkov                                     *
 * Created      :18.10.19                                          *
 * Last Revision:31.10.19                                          *
 * Comment      : the program centers text, (N + delta) - size of modified string  *
@@ -21,27 +21,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<locale.h>
 
-short i = 0;	//counter
+int NMAX = 100;
+int i = 0;	//counter
 /*************************** prototypes ***************************/
 //determine of max line
 int dmxline(FILE* in)
 {
-	short dN = 0;		//right border
-	short dtemp = 0;	//var for compare
-	char dc = 0;		//var for reading from the file
+	int dN = 0;		//right border
+	int dtemp = 0;	//var for compare
+	char dc = 0;	//var for reading from the file
 
-	while (dc != EOF)
+	while (!feof(in))
 	{
 		dtemp = 0;
-		dc = 0;	
-		while ((dc != '\n') && (dc != EOF))
+		dc = 0;
+
+		while ((dc != '\n') && (!feof(in)))
 		{
 			dc = fgetc(in);
+			//if ((dc == '\n') || (feof(in))) break;
 			if (dc == '\t')	dtemp += 5;
-			else dtemp++;
-			if ((dc == '\n') || (dc == EOF)) dtemp++;
+
+			dtemp++;
 		}
+		dtemp--;
 		if (dtemp > dN)	dN = dtemp;
 	}
 	return dN;
@@ -59,13 +64,15 @@ void put_sp(char* pArr, int pN)
 /**************************** main() ******************************/
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	
 	FILE *input = fopen("C:\\Users\\dmitr\\Desktop\\ÏßÂÓ\\3 ÑÅÌ\\ËÐ_3 ÏßÂÓ\\LAB_3_PLHL\\LAB_3_PLHL\\data_0.txt", "rt");
 
 	char* clp;		//clipboard
 	char* spaces;	//
-	short N;		//size of clp 
-	int delta = -1;	//
-	short numsp;	//number of spaces
+	int N;		//size of clp 
+	int delta = 5;		//
+	int numsp;	//number of spaces
 
 	if (input == NULL)
 	{
@@ -80,19 +87,22 @@ int main()
 		system("pause");
 		return 2;
 	}
-	else rewind(input);
 
-	printf("Add the delta: ");
-	scanf("%i", delta);
-	if (delta < 0)
-	{
-		printf("ERROR_3: delta must be > 0");
-		system("pause");
-		return 3;
-	}
+	//printf("Add the delta: ");
+	//scanf("%i", &delta);
+	//if (delta < 0)
+	//{
+	//	printf("ERROR_3: delta must be > 0");
+	//	system("pause");
+	//	return 3;
+	//}
+
+	rewind(input);
 
 	N = dmxline(input);
 	N = N + delta;
+
+	rewind(input);
 
 	clp = malloc(N);
 	spaces = malloc(N);
@@ -103,6 +113,7 @@ int main()
 		cler_line(spaces, N);
 
 		fgets(clp, N, input);
+		clp[strlen(clp) - 1] = '\0';
 		if (clp[0] == '\t')
 		{
 			clp[0] = ' ';
@@ -112,12 +123,15 @@ int main()
 		numsp = numsp / 2;
 		put_sp(spaces, numsp);
 
-		clp = strcat(clp, spaces);
 		clp = strcat(spaces, clp);
+		spaces = malloc(N);
+		cler_line(spaces, N);
+		put_sp(spaces, numsp);
+		clp = strcat(clp, spaces);
 		if (strlen(clp) != N) clp = strcat(clp, " ");
 
-		printf("%s", clp);
-	} while (clp != EOF);
+		printf("%s\n", clp);
+	} while (!feof(input));
 
 	fclose(input);
 
